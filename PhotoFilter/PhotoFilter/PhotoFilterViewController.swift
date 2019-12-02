48509f3b3e2c2141207c6c20e6ce14e55c9befb9
Jerry Haaser <jerry.haaser@hotmail.com>
2019-12-02 12:13:04 -0600

    set up updateView function

--- PhotoFilter/PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilter/PhotoFilterViewController.swift
@@ -7,6 +7,12 @@ class PhotoFilterViewController: UIViewController {
     // Properties
     private let context = CIContext(options: nil)
     private let filter = CIFilter(name: "CIColorControls")! // Can Crash!
+    
+    private var originalImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
 
 	@IBOutlet var brightnessSlider: UISlider!
 	@IBOutlet var contrastSlider: UISlider!
@@ -15,6 +21,9 @@ class PhotoFilterViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        
+        // FIXME: fake if for now
+        originalImage = imageView.image
 
 	}
 	
@@ -34,15 +43,50 @@ class PhotoFilterViewController: UIViewController {
 	// MARK: Slider events
 	
 	@IBAction func brightnessChanged(_ sender: UISlider) {
+        updateView()
 
 	}
 	
 	@IBAction func contrastChanged(_ sender: Any) {
+        updateView()
 
 	}
 	
 	@IBAction func saturationChanged(_ sender: Any) {
+        updateView()
 
 	}
+    
+    // Private functions
+    
+    private func filterImage(_ image: UIImage) -> UIImage {
+        guard let cgImage = image.cgImage else { return image }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        // Set the filter values
+        filter.setValue(ciImage, forKey: "inputImage")
+        filter.setValue(saturationSlider.value, forKey: "inputSaturation")
+        filter.setValue(brightnessSlider, forKey: "inputBrightness")
+        filter.setValue(contrastSlider, forKey: "inputContrast")
+        
+        guard let outputCIImage = filter.outputImage else { return image }
+        
+        let bounds = CGRect(origin: CGPoint.zero, size: image.size)
+        guard let outputCGImage = context.createCGImage(outputCIImage,
+                                                        from: bounds) else { return image }
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateView() {
+        // filter image
+        if let originalImage = originalImage {
+            imageView.image = filterImage(originalImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
 }
 
